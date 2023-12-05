9bb96c7709bba3ad98e43c9fe2d3a89fb6b1ea99
Hermon Assefa <41701546+Hermonj@users.noreply.github.com>
2023-12-05 23:13:18 +0000

    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,39 @@
-
 import streamlit as st
 import pandas as pd
- 
-st.write("""
-# China National Economic Growth Forcasting *system* 
-""")
- 
-df = pd.read_csv("ChinaNationalEconomyData.csv")
-st.line_chart(df)
+import numpy as np
+
+# Read the CSV file
+data = pd.read_csv("ChinaNationalEconomyData.csv")
+
+# Create a simple dashboard with basic plots
+st.title("China's National Economy Dashboard")
+st.subheader("Key Economic Indicators")
+
+# Real GDP Growth
+st.line_chart(data["Real GDP growth (Annual percent change)"])
+
+# GDP per Capita
+st.line_chart(data["GDP per capita, current prices (U.S. dollars per capita)"])
+
+# Inflation Rate
+st.line_chart(data["Inflation rate, average consumer prices (Annual percent change)"])
+
+# Unemployment Rate
+st.line_chart(data["Unemployment rate (Percent)"])
+
+# Current Account Balance
+st.line_chart(data["Current account balance U.S. dollars (Billions of U.S. dollars)"])
+
+st.subheader("Growth Rates and Financial Metrics")
+
+# 5-year GDP Growth Rate
+st.text(data["5-year GDP growth rate"].mean())
+
+# 5-year Inflation Rate
+st.text(data["5-year Inflation rate"].mean())
+
+# 5-year Unemployment Rate
+st.text(data["5-year Unemployment rate"].mean())
+
+# 5-year Current Account Balance
+st.text(data["5-year Current account balance"].mean())
